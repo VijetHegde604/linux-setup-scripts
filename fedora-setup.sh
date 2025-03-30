8aa8d362ec14f4f4f9dbca600f7cac0260c250e5
VijetHegde604 <vijethegde604@gmail.com>
2025-03-31 01:57:46 +0530

    updates

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -56,7 +56,7 @@ remove_bloatware() {
 # Install essential packages
 install_packages() {
     echo "Installing required packages..."
-    run_sudo dnf install -y fastfetch git wget curl fish btop @development-tools libffi-devel || handle_error "Failed to install packages."
+    run_sudo dnf install -y fastfetch git wget curl zsh btop @development-tools libffi-devel ncurses-devel readline-devel sqlite-devel tk-devel gdbm-devel libdb-devel bzip2-devel zlib-devel xz-devel || handle_error "Failed to install packages."
 }
 
 # Install Node.js using nvm
